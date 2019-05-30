9a2b468162f86634804f5d8f1d571ae913b4fa05
Manvitha <manvithareddy69@gmail.com>
2019-05-30 10:56:59 +0530

    First Commit for Jenkins-Example2 Maven Project

---
+++ Jenkins-Example2/src/main/java/com/jenkins/example/Test.java
@@ -0,0 +1,12 @@
+package com.jenkins.example;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello World");
+	}
+
+}
